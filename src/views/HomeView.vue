4b408b4ece364b708915a64950c8a5991feba8c6
sandranii <niyouu63@gmail.com>
2022-10-31 00:50:25 +0800

    fix HomeView: 串接api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,113 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav>
+      <ul>
+        <li
+          v-for="page in pagination"
+          :key="page"
+          :class="{ active: page === currentPage }"
+          @click="triggerPage(page)"
+        >
+          {{ page }}
+        </li>
+      </ul>
+    </nav>
+    <main class="container">
+      <div v-for="item in data.slice(0,page)" :key="item.Id">
+        <h2>{{ item.Name }}</h2>
+        <!-- <p>{{ item.Toldescribe }}</p> -->
+        <!-- <p>{{ item.Description }}</p> -->
+        <!-- <p>{{ item.Id }}</p> -->
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      sourceData: [],
+      data: [],
+      load: false,
+      currentPage: 1, //目前分頁
+      perPage: 100, //一頁幾筆資料
+      pagination: [], // 分頁呈現
+      totalPages: 10, //總頁數
+    };
+  },
+  methods: {
+    getSource() {
+      this.sourceData = [];
+      this.axios
+        .get(
+          "https://api.kcg.gov.tw:443/api/service/Get/9c8e1450-e833-499c-8320-29b36b7ace5c",
+          {
+            headers: {
+              accept: "application/json",
+            },
+            params: {
+              page: this.currentPage,
+              per_page: this.perPage,
+            },
+          }
+        )
+        .then((res) => {
+          this.data = res.data.data.XML_Head.Infos.Info;
+          this.load = false;
+          this.dataListLength = this.data.length;
+          console.log("this.data", this.data);
+          console.log("length", this.dataListLength);
+          this.totalPages = this.dataListLength/this.perPage;
+          this.updatePage();
+        })
+        .catch((error) => console.log("error", error));
+    },
+    triggerPage(page) {
+      this.currentPage = page;
+      this.getSource();
+    },
+    updatePage() {
+      this.pagination = [];
+      let start = 1;
+      let end = this.totalPages;
+      for (let index = start; index <= end; index++) {
+        this.pagination.push(index);
+      }
+    },
+  },
+  mounted() {
+    // this.getData();
+  },
+  created() {
+    this.getSource();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: inline-flex;
+  flex-wrap: wrap;
+  width: 100vw;
+  > div {
+    flex-basis: 20%;
   }
 }
-</script>
+ul {
+  display: inline-flex;
+  li {
+    list-style: none;
+    border: 1px solid #ddd;
+    margin: 0 0.5rem;
+    padding: 0.25rem;
+    &.active {
+      background: rgb(202, 202, 202);
+    }
+  }
+}
+</style>
