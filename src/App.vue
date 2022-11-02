e658e529e1de65947c76af868b8e2d3fb4d9e4a9
sandranii <niyouu63@gmail.com>
2022-11-02 18:00:53 +0800

    fix App.vue css

--- src/App.vue
+++ src/App.vue
@@ -21,9 +21,10 @@ nav {
   a {
     font-weight: bold;
     color: #2c3e50;
+    text-decoration: none;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #fa0; 
     }
   }
 }
